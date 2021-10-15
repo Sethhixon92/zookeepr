abfa8570135c85fb7aafd31c85472656c2cdc0e8
Good Vibes <sethhixon1992@gmail.com>
2021-10-14 23:28:50 -0500

    Error 404

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const app = express();
 
 const { animals } = require('./data/animals');
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
 function filterByQuery(query, animalsArray) {
    let personalityTraitsArray = [];
    //note that we save the animalsArray as filteredResults
@@ -51,6 +55,15 @@ app.get('/api/animals', (req, res) => {
        results = filterByQuery(req.query, results);
    }
     res.json(results);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
 });
 
 app.listen(PORT, () => {
